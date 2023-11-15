fb7225199bb56f490a885cc0fb9188370d02524f
Ansoumane Soumaoro <ansoumanesoumaoro07@gmail.com>
2023-11-15 10:30:32 +0100

    the-wild-oasis v1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const H1=styled.h1{
+  font-size:15px;
+  font-weight:500;
+  background-color:yellow;
+
+}
+function App() {
+  return (
+    <div>
+      <H1>Hello World</H1>
+    </div>
+  );
+}
+
+export default App;
